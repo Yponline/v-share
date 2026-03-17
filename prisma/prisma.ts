bbf89eff7170ed1f4f4f2892729988b03174394f
yesse masawe <yesemassawe0@gmail.com>
2026-03-17 16:40:52 +0300

    setup prisma

---
+++ prisma/prisma.ts
@@ -0,0 +1,21 @@
+// lib/prisma.ts
+import { PrismaClient } from "@prisma/client";
+
+// Global variable to hold the Prisma client instance
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+// Reuse the existing instance if available, otherwise create a new one
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    // You can add logging or other options here
+    // log: ['query', 'info', 'warn', 'error'],
+  });
+
+// In development, store the Prisma client instance on the global object
+// to avoid creating new instances with every hot-reload
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
